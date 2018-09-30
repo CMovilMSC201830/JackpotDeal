6480e8df3324eebb52b776c4de63f6ce3dca1136
nicorema <nicolas.restrepo.mantilla@gmail.com>
2018-09-30 15:03:26 -0500

    Added Home and Login screens

--- SafeZone/app/src/main/java/com/example/nicolsrestrepo/safezone/MainActivity.java
+++ SafeZone/app/src/main/java/com/example/nicolsrestrepo/safezone/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.nicolsrestrepo.safezone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button login;
+    public Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login=(Button) findViewById(R.id.login);
+        signup=(Button) findViewById(R.id.signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            startActivity(new Intent(getBaseContext(),Login.class));
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
