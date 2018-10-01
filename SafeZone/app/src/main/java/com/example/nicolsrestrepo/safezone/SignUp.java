ed1fcd45f354a08b255925c37759995191f425ad
nicorema <nicolas.restrepo.mantilla@gmail.com>
2018-09-30 21:54:06 -0500

    Registration flow to map activity

--- SafeZone/app/src/main/java/com/example/nicolsrestrepo/safezone/SignUp.java
+++ SafeZone/app/src/main/java/com/example/nicolsrestrepo/safezone/SignUp.java
@@ -1,9 +1,14 @@
 package com.example.nicolsrestrepo.safezone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
     public EditText name;
@@ -11,8 +16,7 @@ public class SignUp extends AppCompatActivity {
     public EditText pass;
     public EditText conf_pass;
     public CheckBox checkbox;
-
-
+    public Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,27 @@ public class SignUp extends AppCompatActivity {
         pass=(EditText) findViewById(R.id.signuppass);
         conf_pass=(EditText) findViewById(R.id.signupconfirmpass);
         checkbox = (CheckBox) findViewById(R.id.signupcheck);
+        signup = (Button) findViewById(R.id.signupbtn);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validateForm())
+                    startActivity(new Intent(getBaseContext(),HomeActivity.class));
+            }
+        });
 
+    }
+
+    private boolean validateForm() {
+        if(TextUtils.isEmpty(name.getText().toString()) || TextUtils.isEmpty(phone.getText().toString()) ||
+                TextUtils.isEmpty(pass.getText().toString()) || TextUtils.isEmpty(conf_pass.getText().toString()) || !checkbox.isChecked()){
+            Toast.makeText(getBaseContext(), getString(R.string.allfields), Toast.LENGTH_SHORT).show() ;
+            return false;
+        }else if(!name.getText().toString().equals(phone.getText().toString())){
+            Toast.makeText(getBaseContext(), getString(R.string.nomatchinspwrds), Toast.LENGTH_SHORT).show() ;
+            return false;
+        }
+        return true;
 
     }
 
