5eb42b80cc5823d8a352849bbf5db2b3032289dd
Diego Millan <diegor.millan@gmail.com>
2018-11-11 16:02:01 -0500

    adding date picker and time picker

--- SafeZone/app/src/main/java/com/example/nicolsrestrepo/safezone/ReportEventActivity.java
+++ SafeZone/app/src/main/java/com/example/nicolsrestrepo/safezone/ReportEventActivity.java
@@ -1,5 +1,7 @@
 package com.example.nicolsrestrepo.safezone;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
@@ -7,8 +9,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.nicolsrestrepo.safezone.ObjetosNegocio.EventInformation;
 import com.google.android.gms.maps.model.LatLng;
@@ -17,7 +21,9 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class ReportEventActivity extends AppCompatActivity {
 
@@ -29,6 +35,7 @@ public class ReportEventActivity extends AppCompatActivity {
     private TextView tituloEvento;
     private EditText editText_zona, editText_hora, editText_fecha, editText_detalles;
     private Button button_loadReport;
+    private Calendar myCalendar = Calendar.getInstance();
 
     private final static String USERS_PATH = "usuarios";
     private final static String EVENTS_PATH = "eventos";
@@ -63,6 +70,37 @@ public class ReportEventActivity extends AppCompatActivity {
         editText_fecha = findViewById(R.id.editText_fechaEvento);
         editText_detalles = findViewById(R.id.editText_detallesEvento);
         tituloEvento.setText(evento);
+
+        editText_fecha.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(ReportEventActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        editText_hora.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(ReportEventActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        editText_hora.setText( selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Hora del Evento");
+                mTimePicker.show();
+            }
+        });
 
         button_loadReport.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,5 +139,24 @@ public class ReportEventActivity extends AppCompatActivity {
                 break;
             }
         }
+    }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, i);
+            myCalendar.set(Calendar.MONTH, i1);
+            myCalendar.set(Calendar.DAY_OF_MONTH, i2);
+            updateLabel();
+        }
+    };
+
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yyyy"; // This could be changed
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editText_fecha.setText(sdf.format(myCalendar.getTime()));
     }
 }
